6c64afdc10d9b801dee468226aa6630992f3a1d1
Cloud Mu <muyunsong@gmail.com>
2016-07-08 14:48:35 +0800

    Update dist

--- dist/boilerplate.js
+++ dist/boilerplate.js
@@ -1,13 +1,13 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
+		module.exports = factory(require("react"));
 	else if(typeof define === 'function' && define.amd)
-		define([], factory);
+		define(["React"], factory);
 	else if(typeof exports === 'object')
-		exports["Boilerplate"] = factory();
+		exports["Boilerplate"] = factory(require("react"));
 	else
-		root["Boilerplate"] = factory();
-})(this, function() {
+		root["Boilerplate"] = factory(root["React"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_2__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -52,13 +52,99 @@ return /******/ (function(modules) { // webpackBootstrap
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	// this should be the entry point to your library
+	var _BarChart = __webpack_require__(1);
 	
-	module.exports = {};
+	var _BarChart2 = _interopRequireDefault(_BarChart);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	// this should be the entry point to your library
+	module.exports = {
+	  BarChart: _BarChart2.default
+	};
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var BarChart = function (_Component) {
+	  _inherits(BarChart, _Component);
+	
+	  function BarChart() {
+	    _classCallCheck(this, BarChart);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(BarChart).apply(this, arguments));
+	  }
+	
+	  _createClass(BarChart, [{
+	    key: 'render',
+	    value: function render() {
+	      var width = 400;
+	      var height = 400;
+	      var padding = {
+	        left: 20,
+	        right: 20,
+	        top: 20,
+	        bottom: 20
+	      };
+	      var rectStep = 35;
+	      var rectWidth = 30;
+	      var data = [50, 43, 120, 87, 99, 167, 142];
+	
+	      data = data.map(function (item, index) {
+	        return {
+	          fill: 'steelblue',
+	          x: padding.left + index * rectStep,
+	          y: height - padding.bottom - item,
+	          width: rectWidth,
+	          height: item
+	        };
+	      });
+	
+	      return _react2.default.createElement(
+	        'svg',
+	        { height: height, width: width },
+	        data.map(function (item) {
+	          return _react2.default.createElement('rect', item);
+	        })
+	      );
+	    }
+	  }]);
+	
+	  return BarChart;
+	}(_react.Component);
+	
+	exports.default = BarChart;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
 
 /***/ }
 /******/ ])
