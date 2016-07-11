387ba7b05bb7ee08309f0770a92d0c5cf416b5cc
Cloud Mu <muyunsong@gmail.com>
2016-07-11 16:16:54 +0800

    Fix bundle bug.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import BarChart from './BarChart';
-import ScatterChart from './ScatterChart';
-import LineChart from './LineChart';
-import PieChart from './PieChart';
-import ForceDirectedGraph from './ForceDirectedGraph';
-import ChordDiagram from './ChordDiagram';
-import TreeDiagram from './TreeDiagram';
-import ClusterDiagram from './ClusterDiagram';
+import BarChart from './BarChart.jsx';
+import ScatterChart from './ScatterChart.jsx';
+import LineChart from './LineChart.jsx';
+import PieChart from './PieChart.jsx';
+import ForceDirectedGraph from './ForceDirectedGraph.jsx';
+import ChordDiagram from './ChordDiagram.jsx';
+import TreeDiagram from './TreeDiagram.jsx';
+import ClusterDiagram from './ClusterDiagram.jsx';
 
 // this should be the entry point to your library
 module.exports = {
