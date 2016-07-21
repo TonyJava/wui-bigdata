7d54118bb4326bbcae11414a947af11b9f0590a2
Cloud Mu <muyunsong@gmail.com>
2016-07-21 15:14:15 +0800

    Refact docs.

--- docs/index.jsx
+++ docs/index.jsx
@@ -37,6 +37,104 @@ const pages = [{
       ScatterChart: require('../src/ScatterChart')
     },
     component: require('./ScatterChart.md')
+  }, {
+    path: '/line-chart',
+    title: 'LineChart',
+    imports: {
+      LineChart: require('../src/LineChart')
+    },
+    component: require('./LineChart.md')
+  }, {
+    path: '/pie-chart',
+    title: 'PieChart',
+    imports: {
+      PieChart: require('../src/PieChart')
+    },
+    component: require('./PieChart.md')
+  }, {
+    path: '/force-directed-graph',
+    title: 'ForceDirectedGraph',
+    imports: {
+      ForceDirectedGraph: require('../src/ForceDirectedGraph')
+    },
+    component: require('./ForceDirectedGraph.md')
+  }, {
+    path: '/chord-diagram',
+    title: 'ChordDiagram',
+    imports: {
+      ChordDiagram: require('../src/ChordDiagram')
+    },
+    component: require('./ChordDiagram.md')
+  }, {
+    path: '/tree-diagram',
+    title: 'TreeDiagram',
+    imports: {
+      TreeDiagram: require('../src/TreeDiagram')
+    },
+    component: require('./TreeDiagram.md')
+  }, {
+    path: '/cluster-diagram',
+    title: 'ClusterDiagram',
+    imports: {
+      ClusterDiagram: require('../src/ClusterDiagram')
+    },
+    component: require('./ClusterDiagram.md')
+  }, {
+    path: '/bundle-diagram',
+    title: 'BundleDiagram',
+    imports: {
+      BundleDiagram: require('../src/BundleDiagram')
+    },
+    component: require('./BundleDiagram.md')
+  }, {
+    path: '/pack-diagram',
+    title: 'PackDiagram',
+    imports: {
+      PackDiagram: require('../src/PackDiagram')
+    },
+    component: require('./PackDiagram.md')
+  }, {
+    path: '/histogram',
+    title: 'Histogram',
+    imports: {
+      Histogram: require('../src/Histogram')
+    },
+    component: require('./Histogram.md')
+  }, {
+    path: '/partition-diagram',
+    title: 'PartitionDiagram',
+    imports: {
+      PartitionDiagram: require('../src/PartitionDiagram')
+    },
+    component: require('./PartitionDiagram.md')
+  }, {
+    path: '/stack-diagram',
+    title: 'StackDiagram',
+    imports: {
+      StackDiagram: require('../src/StackDiagram')
+    },
+    component: require('./StackDiagram.md')
+  }, {
+    path: '/treemap',
+    title: 'Treemap',
+    imports: {
+      Treemap: require('../src/Treemap')
+    },
+    component: require('./Treemap.md')
+  }, {
+    path: '/mindmap',
+    title: 'Mindmap',
+    imports: {
+      Mindmap: require('../src/Mindmap')
+    },
+    component: require('./Mindmap.md')
+  }, {
+    path: '/brush-sample',
+    title: 'BrushSample',
+    imports: {
+      BrushSample: require('../src/BrushSample')
+    },
+    component: require('./BrushSample.md')
   }
 ];
 
